8aa48c1fd2d2d97d5b51dcbb4577df3bd3f37d1d
Michael Wood <michaelwood77@live.com>
2021-01-17 21:42:10 +0100

    changed some things, forgot how to do other things

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 const fs = require('fs');
 const http = require('http');
+var readline = require('readline');
+var {google} = require('googleapis');
 const port = 3000;
 
-// import { Controller }  from './models/controller.model';
-// import * as n64Controller  from './controller-keybinds/n64.json';
-// const controls = n64Controller;
+var OAuth2 = google.auth.OAuth2;
+var service = google.youtube('v3');
+var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
 
-const readline = require('readline');
-const {google} = require('googleapis');
-const OAuth2 = google.auth.OAuth2;
-const service = google.youtube('v3');
-const SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
-var TOKEN_DIR = './super-secrets/'                                    // where you saved your token
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/youtube-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.super-secrets/';
 var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
-const TAKE_THIS_API_KEY_OUT = ''
+
+
+var TAKE_THIS_API_KEY_OUT = '';
 
 var authed;
 var running = false;
 var count = 0;
 const VIDEO_ID = process.env.VIDID;               // Your video id
 
-// Load token from local file, authorize client, then call YT's API.
-fs.readFile('client-secret.json', function processClientSecrets(err, content) {
-    if (err) {
-        console.error(`Failed to load secret file (token) -> ${err}`);
-        return;
-    }
-    authorize(JSON.parse(content), getVideoDetails);
-})
+// import { Controller }  from './models/controller.model';
+// import * as n64Controller  from './controller-keybinds/n64.json';
+// const controls = n64Controller;
+
+// Load client secrets from a local file.
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the YouTube API.
+  authorize(JSON.parse(content), getVideoDetails);
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -35,17 +43,14 @@ fs.readFile('client-secret.json', function processClientSecrets(err, content) {
  *
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
- *
- * Source: Google
  */
 function authorize(credentials, callback) {
-    var clientSecret = credentials.installed.client_secret;
-    var clientId = credentials.installed.client_id;
-    var redirectUrl = credentials.installed.redirect_uris[0];
-    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
-    authed = oauth2Client;
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
-      // Check if we have previously stored a token.
+  // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, function(err, token) {
     if (err) {
       getNewToken(oauth2Client, callback);
@@ -63,54 +68,49 @@ function authorize(credentials, callback) {
  * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback to call with the authorized
  *     client.
- *
- * Source: Google
  */
 function getNewToken(oauth2Client, callback) {
-    var authUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
     });
-    console.log('Authorize this app by visiting this url: ', authUrl);
-    var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    rl.question('Enter the code from that page here: ', function(code) {
-      rl.close();
-      oauth2Client.getToken(code, function(err, token) {
-        if (err) {
-          console.log('Error while trying to retrieve access token', err);
-          return;
-        }
-        oauth2Client.credentials = token;
-        storeToken(token);
-        callback(oauth2Client);
-      });
-    });
-  }
+  });
+}
 
-  /**
+/**
  * Store token to disk be used in later program executions.
  *
  * @param {Object} token The token to store to disk.
- *
- * Source: Google
  */
 function storeToken(token) {
-    try {
-      fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-      if (err.code != 'EEXIST') {
-        throw err;
-      }
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
     }
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-      if (err) throw err;
-      console.log('Token stored to ' + TOKEN_PATH);
-    });
   }
-
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    if (err) throw err;
+    console.log('Token stored to ' + TOKEN_PATH);
+  });
+}
 
 /**
  * Lists the names and IDs of up to 10 files.
@@ -118,26 +118,27 @@ function storeToken(token) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function getChannel(auth) {
-    service.channels.list({
-      auth: auth,
-      part: 'snippet,contentDetails,statistics',
-      forUsername: 'GoogleDevelopers'
-    }, function(err, response) {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return;
-      }
-      var channels = response.data.items;
-      if (channels.length == 0) {
-        console.log('No channel found.');
-      } else {
-        console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
-                    'it has %s views.',
-                    channels[0].id,
-                    channels[0].snippet.title,
-                    channels[0].statistics.viewCount);
-      }
-    });
+  var service = google.youtube('v3');
+  service.channels.list({
+    auth: auth,
+    part: 'snippet,contentDetails,statistics',
+    forUsername: 'GoogleDevelopers'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var channels = response.data.items;
+    if (channels.length == 0) {
+      console.log('No channel found.');
+    } else {
+      console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
+                  'it has %s views.',
+                  channels[0].id,
+                  channels[0].snippet.title,
+                  channels[0].statistics.viewCount);
+    }
+  });
 }
 
 // TODO
@@ -147,7 +148,7 @@ function getChannel(auth) {
 function getVideoDetails(auth) {
     service.videos.list({
         auth: auth,
-        id: '36YnV9STBqc',
+        id: VIDEO_ID,
         part: 'snippet,liveStreamingDetails'
     }, function(err, response) {
         if (err) {
