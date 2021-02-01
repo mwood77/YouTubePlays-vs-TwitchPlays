e159a6d3d653b6352ae6dee8229307d776bb5148
Michael Wood <43637076+mwood77@users.noreply.github.com>
2021-02-01 07:58:45 +0100

    some refactoring + fixed duplicate check

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const fs = require('fs');
-var readline = require('readline');
-var {google} = require('googleapis');
-var stream = fs.createWriteStream('output.txt')
+// var fs = require('fs');
+// var readline = require('readline');
+// var {google} = require('googleapis');
+// var stream = fs.createWriteStream('output.txt')
 
-var OAuth2 = google.auth.OAuth2;
-var service = google.youtube('v3');
-var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
+import fs from 'fs';
+import readline from 'readline';
+import {google} from 'googleapis';
+import neek from 'neek';
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/youtube-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+const OAuth2 = google.auth.OAuth2;
+const service = google.youtube('v3');
+
+const SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
+let TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
   process.env.USERPROFILE) + '/.super-secrets/';
-var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+const TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
 const API_KEY = process.env.APIKEY;
 const VIDEO_ID = process.env.VIDID;
 
-var authed;
-var maximumDailyRequests = 10000;
+let authed;
+const maximumDailyRequests = 10000;
+const readable = './resources/unfiltered.txt';
+const writable = './resources/filtered-output.txt';
+const stream = fs.createWriteStream(readable);
 
 
-// var { Controller }  = require('./models/controller');
-// import * as n64Controller  from './controller-keybinds/n64.json';
+// import { Controller } from './models/controller';
+// import * as n64Controller from './controller-keybinds/n64.json';
 // const controls = n64Controller;
 
 /**
@@ -34,25 +39,25 @@ var maximumDailyRequests = 10000;
  *
  */
 const cyrb53 = function(input, seed = 0) {
-  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-  for (let i = 0, ch; i < input.length; i++) {
-      ch = input.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 2654435761);
-      h2 = Math.imul(h2 ^ ch, 1597334677);
-  }
-  h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-  return 4294967296 * (2097151 & h2) + (h1>>>0);
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < input.length; i++) {
+        ch = input.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+    return 4294967296 * (2097151 & h2) + (h1>>>0);
 };
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the YouTube API.
-  authorize(JSON.parse(content), getVideoDetails);
+    if (err) {
+        console.log('Error loading client secret file: ' + err);
+        return;
+    }
+    // Authorize a client with the loaded credentials, then call the YouTube API.
+    authorize(JSON.parse(content), getVideoDetails);
 });
 
 /**
@@ -63,20 +68,20 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+    const clientSecret = credentials.installed.client_secret;
+    const clientId = credentials.installed.client_id;
+    const redirectUrl = credentials.installed.redirect_uris[0];
+    const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
-    } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
-    }
-  });
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
 }
 
 /**
@@ -88,12 +93,12 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
+  const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
   });
   console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
+  let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
@@ -117,17 +122,17 @@ function getNewToken(oauth2Client, callback) {
  * @param {Object} token The token to store to disk.
  */
 function storeToken(token) {
-  try {
-    fs.mkdirSync(TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code !== 'EEXIST') {
+            throw err;
+        }
     }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-    if (err) throw err;
-    console.log('Token stored to ' + TOKEN_PATH);
-  });
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) throw err;
+        console.log('Token stored to ' + TOKEN_PATH);
+    });
 }
 
 /**
@@ -147,12 +152,12 @@ function getVideoDetails(auth) {
           console.error('Encountered an error retrieving video details: ' + err);
           return;
       }
-      let videoDetails = response.data.items[0];
+      const videoDetails = response.data.items[0];
       const channel = videoDetails.snippet.channelTitle
       const chatId = videoDetails.liveStreamingDetails.activeLiveChatId;
       const videoTitle = videoDetails.snippet.title
 
-      if (videoDetails == 0) {
+      if (videoDetails === 0) {
           console.error(`No video with id ${VIDEO_ID} was found.`)
       } else {
           console.log('\n\n======= Source =======\n Channel = %s\n Video = %s\n Live Chat Id = %s\n======================\n\n',
@@ -183,12 +188,12 @@ function getLiveChat(liveChat) {
             return;
         }
         const liveChatDetails = response.data;
-        const totalResults = response.data.pageInfo.totalResults;
+        // const totalResults = response.data.pageInfo.totalResults;
 
-        if (liveChatDetails == 0) {
+        if (liveChatDetails === 0) {
             console.error(`No chat with id ${liveChat} was found.`);
         } else {
-          beginRecursionLogging(liveChat, liveChatDetails.nextPageToken);
+            beginRecursionLogging(liveChat, liveChatDetails.nextPageToken);
         }
     });
 }
@@ -197,7 +202,7 @@ function getLiveChat(liveChat) {
 /**
  * Worker function
  * 
- * Used by recursive call to construct 'output.txt'
+ * Used by recursive call to construct readable text file
  * Must be authenticated in-order to make the call.
  * 
  * API returns paginated results
@@ -220,47 +225,36 @@ function getPaginatedLiveChat(liveChat, nextPageToken) {
         }
 
         const repeat = response.data;
-        if (repeat == 0) {
+        if (repeat === 0) {
             console.error(`No chat with id ${liveChat} was found.`);
         } else {
-          // write output to document
-          repeat.items.forEach((element, index) => {
-
-            const hash = cyrb53(element.snippet.textMessageDetails.messageText)
-
-            stream.write(hash + '=|=' + element.snippet.textMessageDetails.messageText+'\n', 'utf8');
-          })
-          // 'separator tone'
-          // stream.write('\n=====\n=====\n\n')
-          removeDuplicates()
+            // write output to document
+            repeat.items.forEach((element) => {
+                const hash = cyrb53(element.snippet.textMessageDetails.messageText)
+                stream.write(hash + '=|=' + element.snippet.textMessageDetails.messageText+'\n', 'utf8');
+            });
+            removeDuplicates();
         }
     });
 }
 
 function removeDuplicates() {
-  try {
-    var data = fs.readFileSync('output.txt');
-
-    data = data.toString().split(/[\n '=|=']/)
-
-    let result = data.filter((item, pos) => data.indexOf(item) === pos);
-
-    console.info(result);
-    stream.write(result.join('\n'));
-
-  } catch (error) {
-    console.error('Encountered an error when checking output for duplicates: %s', error);
-  }
+    try {
+        // TODO - make this into a stream
+        neek.unique(readable, writable, (result) => {
+            console.info('duplicate check complete \n%s', result);
+        });
+    } catch (error) {
+        console.error('Encountered an error when checking output for duplicates: %s', error);
+    }
 }
   
 function beginRecursionLogging(liveChatID, interval, nextPageToken) {
-
-  (async function loop() {
-    for (let i = 0; i < maximumDailyRequests; i++) {
-      await new Promise(resolve => setTimeout(resolve, 9000));
-      console.info('======== refreshing data @ request number: %s ========', i + 1);
-      getPaginatedLiveChat(liveChatID, nextPageToken);
-    }
-  })();
-
+    (async function loop() {
+        for (let i = 0; i < maximumDailyRequests; i++) {
+            await new Promise(resolve => setTimeout(resolve, 9000));
+            console.info('======== refreshing data @ request number: %s ========', i + 1);
+            getPaginatedLiveChat(liveChatID, nextPageToken);
+        }
+    })();
 }
